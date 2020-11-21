62cef94f4307193daa6864ee7939ca6654841cbd
Rick Wagner <rpwagner@ucsd.edu>
2020-11-20 20:29:16 -0800

    hook for call to mini ca

--- sdsc_jupyter/MiniCAGlobusOAuthenticator.py
+++ sdsc_jupyter/MiniCAGlobusOAuthenticator.py
@@ -1,11 +1,11 @@
 import os
+import subprocess
 from traitlets import Unicode
 from oauthenticator.globus import LocalGlobusOAuthenticator
 
 # to do
 # subclass GlobusOAuthenticator
-# overload authenticate and pre_spawn_hook calls
-# call them, then add steps to get sign certs
+# overload pre_spawn_hook calls
 
 class MiniCAGlobusOAuthenticator(LocalGlobusOAuthenticator):
     mini_ca_scope = Unicode(
@@ -14,8 +14,40 @@ class MiniCAGlobusOAuthenticator(LocalGlobusOAuthenticator):
     def _mini_ca_scope_default(self):
         return os.getenv('MINI_CA_SCOPE', '')
 
+    mini_ca_host = Unicode(
+        help="""Host to send the Mini CA SSH request.""").tag(config=True)
+
+    def _mini_ca_host_default(self):
+        return os.getenv('MINI_CA_HOST', '')
+
+    mini_ca_cmd = Unicode(
+        help="""Mini CA command to use.""").tag(config=True)
+
+    def _mini_ca_cmd_default(self):
+        return os.getenv('MINI_CA_CMD', '/home/ubuntu/minicatokencheck.py')
+    
     async def authenticate(self, handler, data=None):
         if not self.mini_ca_scope in self.scope:
             self.scope.append(self.mini_ca_scope)
         user_info = await super().authenticate(handler, data=data)
+        # strip off mini CA token, save for reuse
+        old_tokens = user_info['auth_state']['tokens']
+        new_tokens = {}
+        mini_ca_token = None
+        for resource_server in old_tokens.keys():
+            if old_tokens[resource_server]['scope'] == self.mini_ca_scope:
+                mini_ca_token = old_tokens[resource_server]['access_token']
+            else
+                new_tokens[resource_server] = old_tokens[resource_server]
+        user_info['auth_state']['tokens'] = new_tokens
+
+        ssh_cmd = '{} {} {}'.format(self.mini_ca_host,
+                                        self.mini_ca_cmd, mini_ca_token)
+        ssh = subprocess.Popen(["ssh", ssh_cmd],
+                                   shell=False,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+        result = ssh.stdout.readlines()
+        if result:
+            user_info['auth_state']['mini_ca'] = result
         return user_info
