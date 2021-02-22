036162b865d297aeaefa7f1b6ad68115f27158f1
Scott Sakai <ssakai@sdsc.edu>
2021-02-22 14:01:34 -0800

    Accept access token on stdin instead of cmdline, load secrets from config file.

--- minicatokencheck.py
+++ minicatokencheck.py
@@ -1,17 +1,25 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import requests
+import configparser
+
+
+# read in the config
+# moved to a config file so we never accidentally check in secrets
+config = configparser.ConfigParser()
+config.read('/dev/shm/globusauth.ini')
+globusconf = config['globus']
 
 # https://docs.globus.org/api/auth/developer-guide/#register-app
-SERVICE_CLIENT_ID = '<Globus client ID goes here>'
-SERVICE_CLIENT_SECRET = '<Globus client secret goes here>'
+SERVICE_CLIENT_ID = globusconf.get('SERVICE_CLIENT_ID')
+SERVICE_CLIENT_SECRET = globusconf.get('SERVICE_CLIENT_SECRET')
 
 # https://docs.globus.org/api/auth/reference/#token-introspect
-INTROSPECTION_URL = 'https://auth.globus.org/v2/oauth2/token/introspect'
+INTROSPECTION_URL = globusconf.get('INTROSPECTION_URL', 'https://auth.globus.org/v2/oauth2/token/introspect')
 
 # Required identity provider domain to get the username from
-IDENTITY_PROVIDER = 'xsede.org'
+IDENTITY_PROVIDER = globusconf.get('IDENTITY_PROVIDER')
 
 def check_token(access_token):
     # Calls the Globus RFC 7662 OAuth 2.0 Token Introspection URL
@@ -33,16 +41,16 @@ def check_token(access_token):
             has_desired_idp = True
             break
     if has_desired_idp:
-        return username
+        return identity['username']
     else:
         return None
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('Usage: minicatokencheck.py <access token>')
-        sys.exit(1)
+    sys.stderr.write("Access Token:\n")
+    token = input()
+
     try:
-        username = check_token(sys.argv[1])
+        username = check_token(token.rstrip())
         if username:
             print(username)
             sys.exit(0)
